f4c1f6fcd079fb78eff768a51c848a83b3bd1f05
Kunal Agarwal <kunal94agarwal@gmail.com>
2018-02-13 06:00:44 +0530

    static square and board component

--- src/app.js
+++ src/app.js
@@ -1,9 +1,38 @@
-class Header extends React.Component {
-  render(){
-    return (
-      <h1>welcome to the coding</h1>
-    )
-  }
-}
 
-ReactDOM.render(<Header />, document.getElementById('app'))
+
+ class Board extends React.Component {
+   renderSquare(i){
+     return <Square value={i}/>
+   }
+   render(){
+     return (
+      <div>
+       <div>
+         {this.renderSquare(0)}
+         {this.renderSquare(1)}
+         {this.renderSquare(2)}
+       </div>
+       <div>
+         {this.renderSquare(3)}
+         {this.renderSquare(4)}
+         {this.renderSquare(5)}
+       </div>
+       <div>
+         {this.renderSquare(6)}
+         {this.renderSquare(7)}
+         {this.renderSquare(8)}
+       </div>
+     </div>
+     )
+   }
+ }
+
+ class Square extends React.Component {
+   render(){
+     return (
+       <button className="square" onClick={() => {alert(this.props.value)}}>{this.props.value}</button>
+     );
+   }
+  }
+
+ ReactDOM.render(<Board />, document.getElementById('app'));
